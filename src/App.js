6c1f4d142d3b3fab7e8dc79c5d8301e5b277da51
aloysiushon <aloysiushon@hotmail.com>
2024-09-15 01:06:48 +0800

    implements sorting tickets

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import TicketForm from "./components/TicketForm";
 import "./styles.css";
 import ticketReducer from "./reducers/ticketReducer";
 import TicketList from "./components/TicketList";
+import { sortTickets } from "./utilities/sortingUtilities";
 
 function App() {
-  const initialState = { tickets: [], editingTicket: null };
+  const initialState = {
+    tickets: [],
+    editingTicket: null,
+    sortPreference: "High to Low",
+  };
 
   const [state, dispatch] = useReducer(ticketReducer, initialState);
   // console.log(state);
   //It returns tickets object inserted in the TicketForm
+
+  const sortedTickets = sortTickets(state.tickets, state.sortPreference);
 
   return (
     <div className="App">
@@ -22,7 +29,18 @@ function App() {
         {state.tickets.length > 0 && (
           <div className="results">
             <h2>All Tickets</h2>
-            <TicketList tickets={state.tickets} dispatch={dispatch} />
+
+            <select
+              value={state.sortPreference}
+              onChange={(e) =>
+                dispatch({ type: "SET_SORTING", payload: e.target.value })
+              }
+            >
+              <option value="High to Low">High to Low</option>
+              <option value="Low to High">Low to High</option>
+            </select>
+
+            <TicketList tickets={sortedTickets} dispatch={dispatch} />
           </div>
         )}
       </div>
